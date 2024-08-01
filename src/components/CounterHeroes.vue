2771534663bfa1145222bfe6e41afb94fe328a29
GBRL <gabrielcarlos91@hotmail.com>
2024-08-01 00:19:54 -0300

    feat: filter items and attr

--- src/components/CounterHeroes.vue
+++ src/components/CounterHeroes.vue
@@ -3,14 +3,22 @@ import { onMounted } from 'vue';
 import { ref } from 'vue';
 import { API_URL, queryGetAllHeroes, stractzApi } from '@/utils'
 import { type IHero } from '@/constants'
+import { reactive } from 'vue';
+import { computed } from 'vue';
 
-const allHeroes = ref();
+const toggleFilter = ref();
 const primaryAttribute = [
     { name: 'agi', type: 'hero_agility' },
     { name: 'str', type: 'hero_strength' },
     { name: 'int', type: 'hero_intelligence' },
     { name: 'all', type: 'hero_universal' },
 ]
+
+const state = reactive({
+    search: null as any,
+    heroes: [] as IHero[],
+    filterAttribute: null as any,
+});
 
 onMounted(async () => {
     await stractzApi
@@ -23,7 +31,7 @@ onMounted(async () => {
         .then((reponse) => {
             const { heroes } = reponse?.data?.data?.constants;
             console.log("DATA", heroes);
-            allHeroes.value = heroes.sort((a: IHero, b: IHero) => a.displayName.localeCompare(b.displayName));
+            state.heroes = heroes.sort((a: IHero, b: IHero) => a.displayName.localeCompare(b.displayName));
         })
         .catch((err) => {
             console.error('Error:', err);
@@ -34,6 +42,35 @@ const getPrimaryAttribute = (attr: string) => {
     return primaryAttribute.find((item) => item.name === attr)?.type ?? '';
 }
 
+
+const heroesFilter = computed(() => {
+    let filteredHeroes = state.heroes;
+
+     if (state.search) {
+        filteredHeroes = filteredHeroes.filter((item) => {
+            return state.search
+                .toLowerCase()
+                .split(" ")
+                .every((v: any) => item.displayName.toLowerCase().includes(v));
+        });
+    }
+
+    if (state.filterAttribute) {
+        filteredHeroes = filteredHeroes.filter((item) => item.stats.primaryAttribute === state.filterAttribute);
+    }
+
+    return filteredHeroes;
+});
+
+const toggleFilterAttr = (attr: string) => {
+    toggleFilter.value = attr.trim();
+    if (state.filterAttribute === attr) {
+        state.filterAttribute = ''; 
+        toggleFilter.value = '';
+    } else {
+        state.filterAttribute = attr;
+    }
+}
 </script>
 
 <template>
@@ -42,39 +79,43 @@ const getPrimaryAttribute = (attr: string) => {
         <div class="flex flex-col justify-center items-center">
             <h1 class="section_title"> Escolha o seu herói</h1>
             <div class="section_desc">
-                <p>Escolha seu herói favorito, conheça as habilidades, atributos e vantagens de cada um. veja informações completa de cada heroi e seus counter heroes com base no meta atual!</p>
+                <p>Escolha seu herói favorito, conheça as habilidades, atributos e vantagens de cada um. veja
+                    informações completa de cada heroi e seus counter heroes com base no meta atual!</p>
             </div>
             <!-- SEARCH -->
             <div class="search_container">
                 <h1>Filtrar heróis</h1>
                 <div class="attr_container">
                     <h1>Atributo</h1>
-                    <div class="attr_image"
+                    <div v-on:click="toggleFilterAttr('str')" :class="toggleFilter === 'str' ? 'toggleFilter': '' " class="attr_image"
                         style="background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-str-active.png&quot;);">
                     </div>
-                    <div class="attr_image"
+                    <div v-on:click="toggleFilterAttr('agi')" :class="toggleFilter === 'agi' ? 'toggleFilter': '' " class="attr_image"
                         style="background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-agi-active.png&quot;);">
                     </div>
-                    <div class="attr_image"
+                    <div v-on:click="toggleFilterAttr('int')" :class="toggleFilter === 'int' ? 'toggleFilter': '' " class="attr_image"
                         style="background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-int-active.png&quot;);">
                     </div>
-                    <div class="attr_image"
+                    <div v-on:click="toggleFilterAttr('all')" :class="toggleFilter === 'all' ? 'toggleFilter': '' " class="attr_image"
                         style="background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-uni-active.png&quot;);">
                     </div>
                 </div>
 
                 <div class="filter">
                     <div class="filter_content">
-                        <div class="filter_img" style="background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/search.svg&quot;);"></div>
-                        <form><input type="text" value=""></form>
+                        <div class="filter_img"
+                            style="background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/search.svg&quot;);">
+                        </div>
+                        <form><input v-model="state.search" type="search"></form>
                     </div>
                 </div>
             </div>
 
             <!-- HEROES -->
-            <div class="grid grid-cols-4 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-6 pb-10 w-full h-full">
+            <div :class="heroesFilter.length === 124 ? 'flex items-center justify-center' : 'flex items-start justify-start'"
+                class="py-4 md:py-5 flex-wrap mt-6 pb-10 main w-full h-full">
                 <div class="group hero_content flex flex-col justify-center items-center shadow-gray-700 transform hover:-translate-y-1 hover:scale-125 hover:z-50 z-10"
-                    v-for="hero in allHeroes" :key="hero.id" :id="hero.id"
+                    v-for="hero in heroesFilter" :key="hero.id"
                     :style="`background-image: url(&quot;https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/${hero.shortName}.png&quot;);left: calc(0% + 122px);top: calc(994px);`">
                     <img v-show="hero.id !== 6"
                         :src="`https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/${hero.shortName}.png`"
@@ -105,9 +146,25 @@ const getPrimaryAttribute = (attr: string) => {
     font-family: "Radiance", "Noto Sans", sans-serif;
 }
 
+.main {
+    /* width: 1200px; */
+    min-height: 1500px;
+    margin-top: 15px;
+    transition-property: height;
+    transition-timing-function: ease-out;
+    transition-duration: .2s;
+    animation-duration: 1s;
+    animation-timing-function: ease-out;
+    animation-delay: .1s;
+    animation-iteration-count: 1;
+    animation-name: NeQOPQgh06ixPcmoyJM01;
+    position: relative;
+}
+
 .section_main {
     width: 100%;
     padding: 0px 100px 0px 100px;
+    min-height: 1500px;
     background-size: 100% auto;
     background-repeat: no-repeat;
     background-position: center top;
@@ -214,7 +271,7 @@ const getPrimaryAttribute = (attr: string) => {
     align-items: center;
 }
 
-.filter_img  {
+.filter_img {
     width: 26px;
     height: 26px;
     min-width: 26px;
@@ -229,6 +286,7 @@ form {
     flex-grow: 1;
     margin-right: 10px;
 }
+
 input {
     height: 30px;
     width: 100%;
@@ -241,7 +299,7 @@ input {
 }
 
 .hero_content {
-    margin-bottom: 5px;
+    margin: 5px;
     box-shadow: 1px 1px 4px #000;
     filter: saturate(0.8);
     transition-property: transform, box-shadow, background-size, opacity, top, left;
@@ -307,5 +365,21 @@ input {
     position: absolute;
     width: 100%;
     height: 100%;
+}
+
+.toggleFilter {
+    filter: brightness(1) saturate(1);
+}
+
+@keyframes NeQOPQgh06ixPcmoyJM01 {
+    0% {
+        transform: translateY(90px);
+        opacity: 0
+    }
+
+    100% {
+        transform: translateY(0px);
+        opacity: 1
+    }
 }
 </style>
