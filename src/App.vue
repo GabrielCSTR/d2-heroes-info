df5c0e362a70ac7336abad74b2f23b14c1e6d682
GBRL <gabrielcarlos91@hotmail.com>
2024-08-01 01:19:09 -0300

    feat: added link forkme

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import CounterHeroes from './components/CounterHeroes.vue';
   <main>
     <CounterHeroes />
   </main>
+  <a class="fixed top-0 right-0" href="https://github.com/GabrielCSTR/dota2-counter-vue"><img
+      src="@/assets/forkme_right_darkblue.png" alt="Fork on Github">
+  </a>
 </template>
 
 <style>
