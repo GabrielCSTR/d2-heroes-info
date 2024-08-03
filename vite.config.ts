d5bde5a7c35c606c6206361904465340f8d76266
GBRL <gabrielcarlos91@hotmail.com>
2024-08-03 19:55:47 -0300

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/d2-heroes-info/",
   publicDir: "/d2-heroes-info/",
   plugins: [
     vue(),
