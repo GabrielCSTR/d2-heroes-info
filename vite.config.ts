f15d8ccf357832072b9c57e76ce27e30f5164fd6
GBRL <gabrielcarlos91@hotmail.com>
2024-08-03 18:51:47 -0300

    fix: added publickdir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: process.env.NODE_ENV === "production" ? "/d2-heroes-info/" : "/",
   plugins: [
     vue(),
   ],
