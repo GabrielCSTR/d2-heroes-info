3579b649985f1666fa5685242f25a26412ea62bb
GBRL <gabrielcarlos91@hotmail.com>
2024-08-03 20:08:17 -0300

    fix: update fork project link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-view :key="$route.path"></router-view>
-    <a class="fixed top-0 right-0" href="https://github.com/GabrielCSTR/dota2-counter-vue"><img
+    <a class="fixed top-0 right-0" href="https://github.com/GabrielCSTR/d2-heroes-info"><img
         src="@/assets/forkme_right_darkblue.png" alt="Fork on Github">
     </a>
   </div>
