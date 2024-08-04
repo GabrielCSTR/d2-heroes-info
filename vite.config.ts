8a2ce57d8621fddc88b51e322484cbceb743e93a
GBRL <gabrielcarlos91@hotmail.com>
2024-08-04 11:56:07 -0300

    fix: update base dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-  ? '/d2-heroes-info/' // Substitua pelo nome do seu repositório
-  : '/',
-  publicDir: "/d2-heroes-info/",
+  base: '/',
   plugins: [
     vue(),
   ],
